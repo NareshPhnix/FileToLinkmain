c33beff95ab73577bd5b2de81b0c133279817f3d
Naresh Kumarr <nareshphnix@gmail.com>
2025-02-09 15:13:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,8 +5,8 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '28192191'))
+API_HASH = environ.get('API_HASH', '663164abd732848a90e76e25cb9cf54a')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
